58e6f0bc6eabade4388bfc4e17df00f6896f730a
Mr.Lin <lsx0044@gmail.com>
2025-11-14 19:28:55 +0800

    添加 webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,6 +2,7 @@ const {VueLoaderPlugin} = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const PrerenderSPAPlugin = require('prerender-spa-plugin-next')
 
+const webpack = require('webpack');
 const path = require("path");
 
 const isProd = (process.env.NODE_ENV === 'production');
@@ -57,6 +58,18 @@ const webpackConfig = {
             inject: true,
             minify: false,
             hash: true
+        })
+    ],
+    new webpack.DefinePlugin({
+            'process.env': JSON.stringify({
+                VUE_APP_META_DESCRIPTION: process.env.VUE_APP_META_DESCRIPTION || "小窝笔记,自动加密的网页版记事本,无需注册.",
+                VUE_APP_META_KEYWORDS: process.env.VUE_APP_META_KEYWORDS || "云记事本, 在线记事本, 带密码的在线记事本, 加密记事本, 网页版记事本, 网络记事本, 在线写字板, 在线文本编辑器, 网页记事本, SimpleNote 替代品, Standard Notes 替代品",
+                VUE_APP_PAGE_TITLE: process.env.VUE_APP_PAGE_TITLE || "小窝笔记 - 在线存储您的笔记",
+                VUE_APP_COMPONENT_TITLE: process.env.VUE_APP_COMPONENT_TITLE || "内容",
+                VUE_APP_APP_KEY: process.env.VUE_APP_APP_KEY || "内容",
+                VUE_APP_H3_TITLE: process.env.VUE_APP_H3_TITLE || "在线储存您的笔记",
+                VUE_APP_FAVICON_URL: process.env.VUE_APP_FAVICON_URL || "https://fastly.jsdelivr.net/gh/ldxw/cdn@master/logo/svg/logo/32x32/favicon.ico"
+            })
         })
     ],
     devServer: {
